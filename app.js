8895ad2caf5d113f538b5bf4240fed55aaea10b4
Viraj Shinde <viraj22shinde@gmail.com>
2023-01-24 18:06:12 +0530

    First Commit.Able to change umbrella colors using buttons

---
+++ app.js
@@ -0,0 +1,13 @@
+
+
+function changeToBlue(){
+    document.getElementById("umbrellaImage").src = './assets/img/Blue umbrella.png';
+}
+
+function changeToPink(){
+    document.getElementById("umbrellaImage").src = './assets/img/Pink umbrella.png';
+}
+
+function changeToYellow(){
+    document.getElementById("umbrellaImage").src = './assets/img/Yello umbrella.png';
+}

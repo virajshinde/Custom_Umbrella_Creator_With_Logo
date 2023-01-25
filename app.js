87982a9bd03a6fade2e4dc38dd23c2bc7c849df0
Viraj Shinde <viraj22shinde@gmail.com>
2023-01-25 16:49:30 +0530

    Second Commit, able to upload img with preview

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+var imgApp = document.querySelector('#app');
 
 function changeToBlue(){
     document.getElementById("umbrellaImage").src = './assets/img/Blue umbrella.png';
@@ -11,3 +11,20 @@ function changeToPink(){
 function changeToYellow(){
     document.getElementById("umbrellaImage").src = './assets/img/Yello umbrella.png';
 }
+
+
+
+
+document.getElementById('imageUpload').addEventListener('change', readURL, true);
+function readURL(){
+    const file = document.getElementById("imageUpload").files[0];
+    const reader = new FileReader();
+    reader.onloadend = function(){
+        document.getElementById('clock').style.backgroundImage = "url(" + reader.result + ")";        
+    }
+    if(file){
+        reader.readAsDataURL(file);
+    }else{
+    }
+}
+

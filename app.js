13bfbe31bb904e8f9c6f447199a1f77818837b25
Viraj Shinde <viraj22shinde@gmail.com>
2023-01-27 18:30:25 +0530

    Eigth Commit.Final Solution

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var imgApp = document.querySelector('#app');
 
-function changeToBlue(){
+// Main function to show Loader on change in Color or Logo Input 
+function loaderIcon(){
     $("#umbrellaImage").addClass("defaultViewLoader")
     $("#mainLogo").addClass("defaultViewLoader")
     $(".loader").removeClass('defaultViewLoader').attr("value",""); 
@@ -12,66 +12,39 @@ function changeToBlue(){
         $("#umbrellaImage").removeClass("defaultViewLoader")
         $("#mainLogo").removeClass("defaultViewLoader")
       }, 3000);
+}
+
+//Individual functions to change color of umbrella
+function changeToBlue(){
+    loaderIcon()
     document.getElementById("umbrellaImage").src = './assets/img/Blue umbrella.png';
 }
 
 function changeToPink(){
-    $("#umbrellaImage").addClass("defaultViewLoader")
-    $("#mainLogo").addClass("defaultViewLoader")
-    $(".loader").removeClass('defaultViewLoader').attr("value",""); 
-    $(".loader").addClass('button_loader').attr("value","");
-    window.setTimeout(function(){
-        $('.loader').removeClass('button_loader').attr("value","\u2713");
-        $(".loader").addClass('defaultViewLoader').attr("value","");
-        $('.loader').prop('disabled', true);
-        $("#umbrellaImage").removeClass("defaultViewLoader")
-        $("#mainLogo").removeClass("defaultViewLoader")
-      }, 3000);
+    loaderIcon()
     document.getElementById("umbrellaImage").src = './assets/img/Pink umbrella.png';
 }
 
 function changeToYellow(){
-    $("#umbrellaImage").addClass("defaultViewLoader")
-    $("#mainLogo").addClass("defaultViewLoader")
-    $(".loader").removeClass('defaultViewLoader').attr("value",""); 
-    $(".loader").addClass('button_loader').attr("value","");
-    window.setTimeout(function(){
-        $('.loader').removeClass('button_loader').attr("value","\u2713");
-        $(".loader").addClass('defaultViewLoader').attr("value","");
-        $('.loader').prop('disabled', true);
-        $("#umbrellaImage").removeClass("defaultViewLoader")
-        $("#mainLogo").removeClass("defaultViewLoader")
-      }, 3000);
+    loaderIcon()
     document.getElementById("umbrellaImage").src = './assets/img/Yello umbrella.png';
 }
 
 
 
-
+//Set a onChange listener to file upload and invoke readURL Function
 document.getElementById('imageUpload').addEventListener('change', readURL, true);
+
+//Main function to upload Image.
 function readURL(){
-	// var s = document.querySelectorAll(".loader").style;
-	// s.opacity = 1;
-	
-	// document.querySelectorAll(".loader").fadeOut("1000");
-    $("#umbrellaImage").addClass("defaultViewLoader")
-    $("#mainLogo").addClass("defaultViewLoader")
-    $(".loader").removeClass('defaultViewLoader').attr("value",""); 
-    $(".loader").addClass('button_loader').attr("value","");
-    window.setTimeout(function(){
-        $('.loader').removeClass('button_loader').attr("value","\u2713");
-        $(".loader").addClass('defaultViewLoader').attr("value","");
-        $('.loader').prop('disabled', true);
-        $("#umbrellaImage").removeClass("defaultViewLoader")
-        $("#mainLogo").removeClass("defaultViewLoader")
-      }, 3000);
+	  loaderIcon()
     const file = document.getElementById("imageUpload").files[0];
     const reader = new FileReader();
     reader.onloadend = function(){
         document.getElementById('clock').style.backgroundImage = "url(" + reader.result + ")";        
     }
     if(file){
-		// (function fade(){(s.opacity-=.1)<0?s.display="none":setTimeout(fade,40)})();
+		
         reader.readAsDataURL(file);
     }else{
 		
